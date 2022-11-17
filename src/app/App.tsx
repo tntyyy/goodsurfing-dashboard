1396bfb77b1a0f2621bfc1faa700894e712f5b36
tnty <tnty.working@gmail.com>
2022-11-17 18:57:59 +0300

    packages: add react-calendar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import styles from "./App.module.scss";
 import Container from "@/components/ui/Container/Container";
 import RequestsContainer from "@/containers/RequestsContainer/RequestsContainer";
 import NotificationsContainer from "@/containers/NotificationsContainer/NotificationsContainer";
+import CalendarContainer from "@/containers/CalendarContainer/CalendarContainer";
 
 const App: FC = () => {
   return (
@@ -20,7 +21,11 @@ const App: FC = () => {
           <RequestsContainer />
         </Container>
       </div>
-      <div className={styles.container__item}>calendar</div>
+      <div className={styles.container__item}>
+          <Container title={"Календарь"}>
+              <CalendarContainer />
+          </Container>
+      </div>
       <div className={styles.container__item}>
         <Container title={"Уведомления"}>
           <NotificationsContainer />
---
+++ src/containers/CalendarContainer/CalendarContainer.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import styles from './CalendarContainer.module.scss';
+
+const CalendarContainer: FC = () => {
+
+  return (
+      <h3>Calendar</h3>
+  );
+};
+
+export default CalendarContainer;
